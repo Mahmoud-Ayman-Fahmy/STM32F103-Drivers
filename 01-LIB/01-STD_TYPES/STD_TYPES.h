51212613811057bba19df9e2a492a7514c4b6c20
Mahmoud Ayman <mahmoudayman7472@gmail.com>
2020-09-01 09:55:37 +0200

    initalize SDK & STD_Types

---
+++ 01-LIB/01-STD_TYPES/STD_TYPES.h
@@ -0,0 +1,15 @@
+/*************************************/
+/* Author     : Mahmoud Ayman        */
+/* Date       : 17 AUG 2020          */
+/* Version    : V01                  */
+/* File Name  : STD_TYPES            */
+/*************************************/
+
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+typedef unsigned char      u8 ;
+typedef unsigned short int u16;				
+typedef unsigned long int  u32;
+
+#endif 
